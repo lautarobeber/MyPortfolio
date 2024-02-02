9d439bdfe2d36727339eaff3bf62afc2dc97e4a0
Lautaro Beber <lautarobeber5a@gmail.com>
2024-02-02 09:23:30 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { Banner } from './components/Banner'
+import { NavBar } from './components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <NavBar/>
+   <Banner/>
+    </>
+  )
+}
+
+export default App
